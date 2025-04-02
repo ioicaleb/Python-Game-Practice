242014723f4ff4ce455bc2b66840afb18af7ffb9
Caleb Gaffney <ioicaleb12@outlook.com>
2025-04-02 10:21:05 -0500

    Add images

---
+++ Balloon Flight/flight.py
@@ -0,0 +1,22 @@
+import pgzrun
+from random import randint
+
+WIDTH = 800
+HEIGHT = 600
+
+balloon = Actor("balloon")
+balloon.pos = 400, 300
+
+bird = Actor("bird")
+bird.pos = randint(800, 1600), randint(10, 200)
+
+house = Actor("house")
+house.pos = randint(800, 1600), 460
+
+tree = Actor("tree")
+tree.pos = randint(800, 1600), 450
+
+def draw():
+    pass
+
+pgzrun.go()
